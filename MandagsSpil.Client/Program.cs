16b26412c7e5f4402f265dd94842626bbf39533f
Kristian Friis <friis1212@gmail.com>
2025-08-17 16:59:52 +0200

    added pwa updater and polish

--- MandagsSpil.Client/Program.cs
+++ MandagsSpil.Client/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MandagsSpil.Client;
 using MudBlazor.Services;
 using MandagsSpil.Client.Layout.AppbarComponents;
+using MudBlazor;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,7 +12,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped<AppbarState>();
 
-builder.Services.AddMudServices();
+builder.Services.AddMudServices(options =>
+{
+    options.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomCenter;
+});
+
+builder.Services.AddPWAUpdater();
+
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
